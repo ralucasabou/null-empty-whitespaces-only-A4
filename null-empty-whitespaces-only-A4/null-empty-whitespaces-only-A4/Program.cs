0f300e33406177861204278dcb0943131752c566
ralucasabou <ralucasabou1996@yahoo.com>
2021-06-03 12:30:01 +0300

    null_empty_whitespaces_only_A4

---
+++ null-empty-whitespaces-only-A4/null-empty-whitespaces-only-A4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace null_empty_whitespaces_only_A4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = ReadFromKeyboard("Text=") ??
+                          ReadFromKeyboard("Text (2nd attempt)=") ??
+                          ReadFromKeyboard("Text (3nd attempt)=") ??
+                          "nu am continut";
+
+            Console.WriteLine(text);
+        }
+
+        static string ReadFromKeyboard(string label)
+        {
+            Console.Write(label);
+            string text = Console.ReadLine();
+
+            return string.IsNullOrWhiteSpace(text)
+                    ? null
+                    : text;
+        }
+    }
+}
+
